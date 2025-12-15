0f84079cfb68ed40d9d6dfeacb6a3403925406c8
Marcelo-Flores-Solis <mfloresso@unsa.edu.pe>
2025-12-14 22:16:27 -0500

    Paso 8: convertir a cadena json y mostrarlo

---
+++ eje8.py
@@ -0,0 +1,43 @@
+
+import json
+
+def main():
+    equipos_futbol = [
+        {
+            "Nombre": "Real Madrid",
+            "país": "España",
+            "nivelAtaque": 95,
+            "nivelDefensa": 90
+        },
+        {
+            "Nombre": "Manchester City",
+            "país": "Inglaterra",
+            "nivelAtaque": 98,
+            "nivelDefensa": 88
+        },
+        {
+            "Nombre": "Bayern Munich",
+            "país": "Alemania",
+            "nivelAtaque": 94,
+            "nivelDefensa": 85
+        },
+        {
+            "Nombre": "Melgar",
+            "país": "Perú",
+            "nivelAtaque": 75,
+            "nivelDefensa": 78
+        }
+    ]
+
+    print("--- Objeto Python (Lista de Diccionarios) ---")
+    print(equipos_futbol) 
+    
+    
+    json_str = json.dumps(equipos_futbol, indent=4, ensure_ascii=False)
+
+    print("\n--- Cadena JSON Formateada ---")
+    print(json_str)
+
+
+if __name__ == "__main__":
+    main()
